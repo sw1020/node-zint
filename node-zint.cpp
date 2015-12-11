f5efa8a7538408e5e2f3367e845e2490e8f9ba74
Stephen Wang <wangchao.stephen@gmail.com>
2015-12-11 09:51:20 +0800

    Init cpp file

---
+++ node-zint.cpp
@@ -0,0 +1,8 @@
+//
+// Created by stephen on 15/12/11.
+//
+
+#include "node-zint.h"
+#include <node.h>
+
+
---
+++ node-zint.h
@@ -0,0 +1,8 @@
+//
+// Created by stephen on 15/12/11.
+//
+
+#ifndef NODE_ZINT_NODE_ZINT_H
+#define NODE_ZINT_NODE_ZINT_H
+
+#endif //NODE_ZINT_NODE_ZINT_H
