1c5f201e42d2d54d115b4610ba7c7b75fe7a15b1
Stephen Wang <stephen@smartac.co>
2015-12-14 16:59:32 +0800

    finish image resize algorithm

--- node-zint.cpp
+++ node-zint.cpp
@@ -7,6 +7,7 @@
 #include <gd.h>
 #include "./zint_lib/zint.h"
 #include "nan.h"
+#include <cstdlib>
 
 namespace node_zint {
     using v8::FunctionCallbackInfo;
@@ -50,9 +51,9 @@ namespace node_zint {
             size = args[1]->NumberValue();
 
         String::Utf8Value param1(args[2]->ToString());
-        std::string fg_color = std::string(*param1);
+        std::string s_fg_color = std::string(*param1);
         String::Utf8Value param2(args[3]->ToString());
-        std::string bg_color = std::string(*param2);
+        std::string s_bg_color = std::string(*param2);
         String::Utf8Value param3(args[4]->ToString());
         std::string txt = std::string(*param3);
 
@@ -71,7 +72,8 @@ namespace node_zint {
         struct zint_symbol *my_symbol;
         my_symbol = ZBarcode_Create();
         my_symbol->symbology = symbology;
-        //my_symbol->option_1 = ecc_level;
+        //if(symbology==BARCODE_QRCODE)
+            my_symbol->option_1 = ecc_level;
         //my_symbol->
 
 
@@ -93,39 +95,123 @@ namespace node_zint {
 
             int img_stream_len = 0;
             gdImagePtr img;
-            int code_size = size/my_symbol->bitmap_width;
-            code_size = (code_size == 0) ? 1 : code_size;
+            //int code_size = (size-margin*2)/my_symbol->bitmap_width;
+            //code_size = (code_size == 0) ? 1 : code_size;
             int img_margin = margin;
+            int img_width = 0;
+            int img_height = 0;
+            float extend = 1.0;
 
+            if( (size-margin*2)<my_symbol->bitmap_width ){
+                img_width = my_symbol->bitmap_width+img_margin*2;
+                img_height = my_symbol->bitmap_height+img_margin*2;
+                extend = 1.0;
+            }
+            else{
+                img_width = size;
+                extend = (float)(img_width-img_margin*2)/(float)my_symbol->bitmap_width;
+                img_height = (int)(extend*(float)my_symbol->bitmap_height)+img_margin*2; // +0.5
 
-            int img_width = my_symbol->bitmap_width * code_size + 2 * img_margin;
-            int img_height = my_symbol->bitmap_height * code_size + 2 * img_margin;
+            }
+
+            //int img_width = my_symbol->bitmap_width * code_size + 2 * img_margin;
+            //int img_height = my_symbol->bitmap_height * code_size + 2 * img_margin;
 
             img = gdImageCreate(img_width, img_height);
 
-            int fg_color = gdImageColorAllocate(img, 0, 0, 0);
+            // extract rgb color from string
+            int fg_r = 0, fg_g = 0, fg_b = 0;
+            if( s_fg_color.size()>=6 ) {
+                char ss[3];
+                ss[2] = '\0';
+                char *pEnd;
+                ss[0] = s_fg_color[0];
+                ss[1] = s_fg_color[1];
+                fg_r = std::strtol(ss,&pEnd,16);
 
-            int bg_color = gdImageColorAllocate(img, 255, 255, 255);
+                ss[0] = s_fg_color[2];
+                ss[1] = s_fg_color[3];
+                fg_g = std::strtol(ss,&pEnd,16);
+
+                ss[0] = s_fg_color[4];
+                ss[1] = s_fg_color[5];
+                fg_b = std::strtol(ss,&pEnd,16);
+            }
+
+            int fg_color = gdImageColorAllocate(img, fg_r, fg_g, fg_b);
+
+            int bg_r = 0, bg_g = 0, bg_b = 0;
+            if( s_bg_color.size()>=6 ) {
+                char ss[3];
+                ss[2] = '\0';
+                char *pEnd;
+                ss[0] = s_bg_color[0];
+                ss[1] = s_bg_color[1];
+                bg_r = std::strtol(ss,&pEnd,16);
+
+                ss[0] = s_bg_color[2];
+                ss[1] = s_bg_color[3];
+                bg_g = std::strtol(ss,&pEnd,16);
+
+                ss[0] = s_bg_color[4];
+                ss[1] = s_bg_color[5];
+                bg_b = std::strtol(ss,&pEnd,16);
+            }
+            int bg_color = gdImageColorAllocate(img, bg_r, bg_g, bg_b);
 
             gdImageFill(img, 0, 0, bg_color);
 
 
             int i = 0, posx, posy, x, y, red, blue, green;
+
+            int preX = 0, preY = 0;
+            int nextX = 0 ,nextY = 0;
+
             for (y = 0; y < my_symbol->bitmap_height; y++) {
+                nextY = preY+extend;
+                while(true){
+                    if( int(nextY/extend)>y ){
+                        nextY--;
+                    }
+                    else
+                        break;
+                }
+                preX = 0;
+                nextX = 0;
                 for (x = 0; x < my_symbol->bitmap_width; x++) {
+                    nextX = preX+extend;
+                    while(true){
+                        if( int(nextX/extend)>x ){
+                            nextX--;
+                        }
+                        else
+                            break;
+                    }
+
                     red = (int) my_symbol->bitmap[i];
                     green = (int) my_symbol->bitmap[i + 1];
                     blue = (int) my_symbol->bitmap[i + 2];
 
-                    if (red == 0 && green == 0 && blue == 0) {
-                        posx = x * code_size + img_margin;
-                        posy = y * code_size + img_margin;
+                    if (red == 0 && green == 0 && blue == 0){
 
-                        gdImageFilledRectangle(img, posx, posy, posx + code_size, posy + code_size, fg_color);
+//                        posx = (x*extend)+img_margin;
+//                        posy = (y*extend)+img_margin;
+//                        gdImageFilledRectangle(img, posx, posy, posx + extend, posy + extend, fg_color);
+//                        posx = x * code_size + img_margin;
+//                        posy = y * code_size + img_margin;
+//
+//                        gdImageFilledRectangle(img, posx, posy, posx + code_size, posy + code_size, fg_color);
+
+
+
+                        gdImageFilledRectangle(img, preX+img_margin, preY+img_margin, nextX+img_margin, nextY+img_margin, fg_color);
                     }
                     i += 3;
+                    preX = nextX+1;
 
                 }
+
+                preY = nextY+1;
             }
 
             img_stream = gdImagePngPtr(img, &img_stream_len);
